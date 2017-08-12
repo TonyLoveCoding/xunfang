692868a392c7db720e011b94eb7426d814f3c998
TonyLoveCoding <765534395@qq.com>
2017-08-12 10:16:22 +0800

    前端首页的初步布置

---
+++ src/main/java/team/etop/xunfang/home/web/HomeController.java
@@ -0,0 +1,21 @@
+package team.etop.xunfang.home.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Tony Yao on 2017/8/12.
+ */
+//前端访客的控制器
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping
+    public ModelAndView initIndex(){
+        return new ModelAndView("/home/index");
+    }
+
+
+}
--- src/main/webapp/WEB-INF/jsp/system/common.jsp
+++ src/main/webapp/WEB-INF/jsp/system/common.jsp
@@ -7,5 +7,5 @@
 <!-- Jquery -->
 <script src="/assets/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
 <!-- Bootstrap -->
-<link href="/assets/bookstrap/css/bootstrap.min.css" rel="stylesheet"/>
-<script src="/assets/bookstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+<script src="/assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
