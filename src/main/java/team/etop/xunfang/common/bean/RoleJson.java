950880b6ce80cebd4125c720078f01999cd497dc
ZYZ <1352646673@qq.com>
2017-08-16 16:59:46 +0800

    测试

---
+++ src/main/java/team/etop/xunfang/common/bean/RoleJson.java
@@ -0,0 +1,18 @@
+package team.etop.xunfang.common.bean;
+
+import team.etop.xunfang.modules.po.Role;
+
+/**
+ * Created by ZYZ on 2017/8/14
+ */
+public class RoleJson {
+    Role role;
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role){
+        this.role=role;
+    }
+    
+}
