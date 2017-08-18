f4da3de40a7650bcfae71dbf11f68bfc6c74f049
TonyLoveCoding <765534395@qq.com>
2017-08-18 15:37:15 +0800

    首页添加标签搜索和关键词搜索

--- src/main/webapp/assets/home/index/js/index.js
+++ src/main/webapp/assets/home/index/js/index.js
@@ -6,5 +6,54 @@ $(function(){
         $(this).removeClass('hover');
         $(this).children('.hoverBlock').css('display','none');
     });
+
+    $(".select-location li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_location").val(val);
+        $("#form_search").submit();
+    });
+
+    $(".select-type li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_type").val(val);
+        $("#form_search").submit();
+
+    });
+
+    $(".select-houseType li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_houseType").val(val);
+        $("#form_search").submit();
+    });
+
+    $(".select-total-prices li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_totalPrices").val(val);
+        $("#form_search").submit();
+    });
+
+    $(".select-unit-price li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_unitPrice").val(val);
+        $("#form_search").submit();
+    });
+
+    $(".select-feature li a").click(function () {
+        var val = $(this).attr("name");
+        $("#inp_hid_feature").val(val);
+        $("#form_search").submit();
+    });
+
+    $("#search-button").click(function () {
+        var keyword = $("#search-input").val();
+        $("#inp_hid_keyword").val(keyword);
+        $("#form_search").submit();
+    });
+
+    $("#search-button-hover").click(function () {
+        var keyword = $("#search-input-hover").val();
+        $("#inp_hid_keyword").val(keyword);
+        $("#form_search").submit();
+    });
 });
 
